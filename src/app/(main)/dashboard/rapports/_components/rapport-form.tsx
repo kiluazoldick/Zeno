62cc14bb13d00bc4ef6c67ba5ec7c46f0f0fd38e
kiluazoldick <nangadoumer@gmail.com>
2026-07-02 15:59:48 +0100

    🐛 Fix: Correction de rapport form

--- src/app/(main)/dashboard/rapports/_components/rapport-form.tsx
+++ src/app/(main)/dashboard/rapports/_components/rapport-form.tsx
@@ -211,7 +211,6 @@ export function RapportForm() {
                                 );
                               }
                             }}
-                            initialFocus
                           />
                         </PopoverContent>
                       </Popover>
